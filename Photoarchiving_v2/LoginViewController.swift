a73bfdd3094012a37a467cd88dbc8e403a850037
Tony Forsythe <forsythetony@gmail.com>
2016-07-06 18:12:43 -0500

    Adds home and login view controllers

---
+++ Photoarchiving_v2/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Photoarchiving_v2
+//
+//  Created by Tony Forsythe on 7/6/16.
+//  Copyright © 2016 Tony Forsythe. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
