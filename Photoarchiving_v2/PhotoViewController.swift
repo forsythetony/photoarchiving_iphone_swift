058ce52c9a3af371123f4ae5a950c32ddfd00da6
Tony Forsythe <forsythetony@gmail.com>
2016-07-06 22:17:40 -0500

    Mostly working

---
+++ Photoarchiving_v2/PhotoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoViewController.swift
+//  Photoarchiving_v2
+//
+//  Created by Tony Forsythe on 7/6/16.
+//  Copyright © 2016 Tony Forsythe. All rights reserved.
+//
+
+import Cocoa
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView : UIImageView!
+    
+    
+    
+    var photoInfo : TFPhoto?
+    
+    
+}
